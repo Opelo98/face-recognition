c0b2eaabf5b1aaed96648732100849e6417e7f3d
Opelo98 <84423325+Opelo98@users.noreply.github.com>
2021-05-20 00:14:06 +0200

    facial recognition

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Emgu.CV;
+using System;
 using System.Windows.Forms;
+using VisioForge.Shared.Accord.Vision.Detection;
+
 
 namespace FaceRecognition111
 {
     public partial class Form1 : Form
     {
+        //Declare variables
+        HaarCascade faceDetected;
         public Form1()
         {
             InitializeComponent();
+            faceDetected = new HaarCascade("haarcascade_frontalface_default.xml.xml");
+        }
+
+        private void start_click(object sender, EventArgs e)
+        {
+
         }
 
     }
